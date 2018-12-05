5fccd8a70a3ffda24a9241125cd54f4edde9abc8
Akin Ünal <akin@posteo.de>
2018-12-05 17:43:26 +0100

    probs

---
+++ Assistment/Extensions/ColorMatrix.cs
@@ -0,0 +1,82 @@
+﻿namespace Assistment.Extensions
+{
+    public class ColorMatrix
+    {
+        /// <summary>
+        /// ARGB x 1ARGB
+        /// </summary>
+        public float[,] Values { get; set; }
+
+        public ColorMatrix()
+        {
+            Values = new float[4, 5];
+            for (int i = 0; i < 4; i++)
+                Values[i, i] = 1;
+        }
+
+        public float this[int output, int input]
+        {
+            get { return Values[output, input]; }
+            set { Values[output, input] = value; }
+        }
+
+        public ColorF Apply(ColorF Input)
+        {
+            ColorF Output = new ColorF();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                    Output[i] += Values[i, j] * Input[j];
+                Output[i] += Values[i, 4];
+            }
+            return Output;
+        }
+
+        public static ColorMatrix Projection(params ColorF[] Space)
+        {
+            Space = ColorF.GramSchmidt(Space);
+            ColorMatrix cm = new ColorMatrix();
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                {
+                    cm[i, j] = 0;
+                    for (int l = 0; l < Space.Length; l++)
+                        cm[i, j] += Space[l][i] * Space[l][j];
+                }
+
+            return cm;
+        }
+        public static ColorMatrix Projection(float Alpha, params ColorF[] Space)
+        {
+            ColorF[] FlatSpace = Space.DeepClone();
+            for (int i = 0; i < FlatSpace.Length; i++)
+                FlatSpace[i][0] = 0;
+
+            Space = ColorF.GramSchmidt(FlatSpace);
+            ColorMatrix cm = new ColorMatrix();
+            for (int i = 1; i < 4; i++)
+                for (int j = 1; j < 4; j++)
+                {
+                    cm[i, j] = 0;
+                    for (int l = 0; l < Space.Length; l++)
+                        cm[i, j] += Space[l][i] * Space[l][j];
+                }
+            cm[0, 0] = Alpha;
+            cm[0, 4] = 0;
+
+            return cm;
+        }
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < 4; i++)
+            {
+                s += "|";
+                for (int j = 0; j < 5; j++)
+                    s += " " + Values[i, j].ToString("F3");
+                s += "|\r\n";
+            }
+            return s;
+        }
+    }
+}
---
+++ Assistment/Extensions/GraphicsExtender.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+
+namespace Assistment.Extensions
+{
+    public static class GraphicsExtender
+    {
+        public static void Raise(this Graphics Graphics)
+        {
+            Graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            Graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+        }
+        public static void Lower(this Graphics Graphics)
+        {
+            Graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighSpeed;
+            Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Low;
+            Graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighSpeed;
+            Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighSpeed;
+            Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixel;
+        }
+
+        public static void DrawFillEllipse(this Graphics g, Pen pen, Brush brush, RectangleF rectangle)
+        {
+            if (brush != null)
+                g.FillEllipse(brush, rectangle);
+            if (pen != null)
+                g.DrawEllipse(pen, rectangle);
+        }
+    }
+}
