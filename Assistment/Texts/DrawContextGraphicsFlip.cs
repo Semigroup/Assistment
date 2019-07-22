aa49e4d3416d6387b89b701be214b22d67fbecae
Akin <akin10@t-online.de>
2019-07-22 21:47:02 +0200

    flipped y draw context

---
+++ Assistment/Texts/DrawContextGraphicsFlip.cs
@@ -0,0 +1,58 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+namespace Assistment.Texts
+{
+    public class DrawContextGraphicsFlip : DrawContextGraphics
+    {
+        public bool FlippedX { get; private set; }
+        public RectangleF Frame { get; private set; }
+        public float Scaling { get; private set; }
+
+        public DrawContextGraphicsFlip(Graphics g, RectangleF Frame, float Scaling)
+          : base(g)
+        {
+            this.Frame = Frame;
+            this.Scaling = Scaling;
+        }
+        public DrawContextGraphicsFlip(Graphics g, RectangleF Frame, float Scaling, Brush backcolor, float Bildhohe)
+            : base(g, backcolor, Bildhohe)
+        {
+            this.Frame = Frame;
+            this.Scaling = Scaling;
+        }
+
+        public void FlipX()
+        {
+            if (FlippedX)
+            {
+                g.ResetTransform();
+                g.ScaleTransform(Scaling, Scaling);
+                g.TranslateTransform(this.Frame.Left, this.Frame.Top);
+                FlippedX = false;
+            }
+            else
+            {
+                g.ResetTransform();
+                g.ScaleTransform(-Scaling, Scaling);
+                g.TranslateTransform(-this.Frame.Right, this.Frame.Top);
+                FlippedX = true;
+            }
+        }
+        public override void DrawString(string text, Font font, Brush brush, float x, float y, float height)
+        {
+            if (FlippedX)
+            {
+                FlipX();
+                SizeF size = g.MeasureString(text, font);
+                x = Frame.Right - x - size.Width;
+                base.DrawString(text, font, brush, x, y, height);
+
+                FlipX();
+            }
+            else
+                base.DrawString(text, font, brush, x, y, height);
+        }
+    }
+}
